69318691e40142beb3bb967e2c934ea1b3400c00
ionlyAamir <amir_233012@saitm.ac.in>
2025-04-11 10:53:03 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,110 @@
+if(process.env.NODE_ENV != "production") {
+  require('dotenv').config()
+}
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methoOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/reivew.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const userRouter = require("./routes/user.js");
+
+
+
+main()
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/Maquan");
+}
+
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(methoOverride("_method"));
+app.engine("ejs", ejsMate);
+
+
+//session ka concept use kiya hai yha
+const sessionOptions = {//flash
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+
+};
+
+app.get("/", (req, res) => {
+  res.send("this is root path");
+});
+
+
+app.use(session(sessionOptions));//flash
+//ba hm use connect-flash ke concept ko
+app.use(flash());//flash
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.use((req, res, next) => {//flash
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student"
+//   });
+//   let registerUser = await User.register(fakeUser, "helloworld");
+//   res.send(registerUser);
+// })
+
+//express router
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
+
+
+
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "page not found!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  //res.status(statusCode).send(message);
+});
+
+app.listen(port, () => {
+  console.log(`listening on this port${port}`);
+});
